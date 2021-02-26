5327d1c388871018e1c8056b2218b10299e7efc1
gv706 <chandragvn2000@gmail.com>
2021-02-26 14:59:45 +0530

    modified matplotlib

--- matplotlib_with_opencv.py
+++ matplotlib_with_opencv.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 
 img=cv2.imread("MyPicture.png",-1)
 cv2.imshow("Image",img)
+print(img.shape)
+# Generally opencv reads the image in bgr format while the matplotlib reads the image in rgb format
+# so we need to convert the bgr to rgb
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
+plt.xticks([]),plt.yticks([])
+# above is to hide the ticks
 plt.imshow(img)
 plt.show()
 
